ed78ccf6bc57948ac974f0de362b8da1d3f8e22d
Dana Khairallah <danamohammd247@gmail.com>
2025-06-21 14:58:17 +0300

    Update challenges

--- lib/widget/challenges.dart
+++ lib/widget/challenges.dart
@@ -2,6 +2,7 @@ import 'package:diy_challenge_app/screens/challenge_details_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/challenge_provider.dart';
+import '../utils/app_localizations.dart';
 
 class ChallengeTabContent extends StatelessWidget {
   const ChallengeTabContent({super.key});
@@ -9,6 +10,7 @@ class ChallengeTabContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final challenges = Provider.of<ChallengeProvider>(context).challenges;
+    final loc = AppLocalizations.of(context)!;
 
     return Padding(
       padding: const EdgeInsets.all(30),
@@ -22,9 +24,20 @@ class ChallengeTabContent extends StatelessWidget {
         ),
         itemBuilder: (ctx, i) {
           final ch = challenges[i];
-          return Card(
-            elevation: 20,
-            child: SingleChildScrollView(
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => ChallengeDetails(challengeId: ch.id),
+                ),
+              );
+            },
+            child: Card(
+              elevation: 20,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
@@ -35,48 +48,34 @@ class ChallengeTabContent extends StatelessWidget {
                     child: Image.asset(
                       ch.imageUrl,
                       height: 100,
+                      width: double.infinity,
                       fit: BoxFit.cover,
                     ),
                   ),
                   const SizedBox(height: 10),
-
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          ch.title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            loc.translate(ch.title),
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
                           ),
-                        ),
-                        const SizedBox(height: 2),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-
-                          children: [
-                            Text(
-                              "${ch.rating} ⭐",
-                              style: const TextStyle(fontSize: 13),
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.info_outline, size: 20),
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder:
-                                        (_) =>
-                                            ChallengeDetails(challengeId: ch.id),
-                                  ),
-                                );
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
+                          const SizedBox(height: 8),
+                          Text(
+                            "${ch.rating} ⭐",
+                            style: const TextStyle(fontSize: 13),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
