863859162f88c10d593af4e3c66855c311a21475
Hourieh Jeibawi <163925355+Hourieh2001@users.noreply.github.com>
2025-06-21 15:27:11 +0300

    Update topRated.dart

--- lib/screens/topRated.dart
+++ lib/screens/topRated.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/cupertino.dart';
 
+class TopRated extends StatefulWidget {
+  const TopRated({super.key});
+
+  @override
+  State<TopRated> createState() => _TopRatedState();
+}
+
+class _TopRatedState extends State<TopRated> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
