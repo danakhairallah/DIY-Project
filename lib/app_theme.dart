57f87b44f3451211ae380a186f27f1997eb3f199
Dana Khairallah <danamohammd247@gmail.com>
2025-06-21 14:58:51 +0300

    Update app_theme

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -4,29 +4,29 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
-    primaryColor: const Color(0xFFC62828), // نهدي غامق
-    scaffoldBackgroundColor: const Color(0xFFFFCDD2), // زهري فاتح
+    primaryColor: const Color(0xFF2083B8),
+    scaffoldBackgroundColor: const Color(0xFFBEDAE1),
     appBarTheme: const AppBarTheme(
-      backgroundColor: Color(0xFFC62828), // زهري غامق
+      backgroundColor: Color(0xFF2083B8),
       foregroundColor: Colors.white,
       elevation: 0,
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFFC62828),
+        backgroundColor: const Color(0xFF2083B8),
         foregroundColor: Colors.white,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         ),
       ),
     ),
-    cardColor: const Color(0xFFFFCDD2), // لون الكروت - نهدي فاتح
+    cardColor: const Color(0xFFBEDAE1),
     textTheme: const TextTheme(
       bodyMedium: TextStyle(color: Colors.black87, fontSize: 14),
       titleLarge: TextStyle(
         fontSize: 20,
         fontWeight: FontWeight.bold,
-        color: Color(0xFFC62828),
+        color: Color(0xFF2083B8),
       ),
     ),
   );
