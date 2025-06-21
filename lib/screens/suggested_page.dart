389c18be8694b3472513162a6eb572f945ae88f1
bara'ah <baraaajameha@gmail.com>
2025-06-21 23:32:02 +0300

    transate SuggestedPage

--- lib/screens/suggested_page.dart
+++ lib/screens/suggested_page.dart
@@ -1,40 +1,43 @@
+import 'package:diy_challenge_app/utils/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 class SuggestedPage extends StatelessWidget {
   const SuggestedPage({super.key});
 
-  final List<Map<String, String>> suggestions = const [
-    {
-      'title': 'Create a Mini Lamp',
-      'description': 'Use simple materials to make a decorative lamp.',
-      'image': 'assets/images/lamp.jpg',
-    },
-    {
-      'title': 'DIY Plant Pot',
-      'description': 'Reuse bottles to grow your plant.',
-      'image': 'assets/images/plant.jpg',
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final loc = AppLocalizations.of(context)!;
+
+    final List<Map<String, String>> suggestions = [
+      {
+        'title': loc.translate("mini_lamp_title"),
+        'description': loc.translate("mini_lamp_description"),
+        'image': 'assets/images/lamp.jpg',
+      },
+      {
+        'title': loc.translate("plant_pot_title"),
+        'description': loc.translate("plant_pot_description"),
+        'image': 'assets/images/plant.jpg',
+      },
+    ];
+
     return Scaffold(
-      appBar: AppBar(title: Text('Suggested Challenges')),
+      appBar: AppBar(title: Text(loc.translate("suggested_challenges"))),
       body: ListView.builder(
         itemCount: suggestions.length,
         itemBuilder: (context, index) {
-          final suggestion = suggestions[index];
+          final item = suggestions[index];
           return Card(
-            margin: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(12),
             child: ListTile(
-              leading: Image.asset(suggestion['image']!),
-              title: Text(suggestion['title']!),
-              subtitle: Text(suggestion['description']!),
+              leading: Image.asset(item['image']!, width: 60),
+              title: Text(item['title']!),
+              subtitle: Text(item['description']!),
               trailing: ElevatedButton(
                 onPressed: () {
-                  // Navigate to challenge details
+              Navigator.pushNamed(context, '/upload', arguments: item);
                 },
-                child: Text('Start'),
+                child: Text(loc.translate("start")),
               ),
             ),
           );
