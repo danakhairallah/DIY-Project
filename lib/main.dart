2f8ffbdf515194fae690fcea05d708e37dd4c048
Hourieh Jeibawi <163925355+Hourieh2001@users.noreply.github.com>
2025-06-23 13:20:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,10 @@ import 'package:diy_challenge_app/utils/dark_theme_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+Future<void> main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
